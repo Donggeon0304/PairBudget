5f983c970b9f33706b5bd8f78d30b3d7383ae655
rodzl <rodzld18@naver.com>
2026-07-13 14:36:22 +0900

    chore: 루트 불필요 파일 정리 (임시 스크립트/테스트 23개 삭제)

--- test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('D:/PairBudget/src/services/BankNotificationParser.ts', 'utf8');
-const match = content.match(/function parseMerchantGeneric[\s\S]+?(?=export function isBankSms)/);
-console.log(match[0]);
--- test_arrow.ts
+++
@@ -1,3 +0,0 @@
-import { parseNotification } from './src/services/BankNotificationParser';
-
-console.log(parseNotification('카카오뱅크 입금 1원\n명동건 → 입출금통장(1893)', 'com.kakaobank.channel'));
